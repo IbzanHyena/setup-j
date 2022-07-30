e47afac71ea027c089604ca4dc8079b7a87da39a
Ibzan <ibzan@ibzan.co.uk>
2022-07-30 15:12:45 +0100

    Use HTTPS

--- setup.sh
+++ setup.sh
@@ -2,17 +2,14 @@
 
 set -euxo pipefail
 
-wget "http://www.jsoftware.com/download/j${J_VERSION}/install/j${J_VERSION}_linux64.tar.gz"
+wget "https://www.jsoftware.com/download/j${J_VERSION}/install/j${J_VERSION}_linux64.tar.gz"
 tar -xzf "j${J_VERSION}_linux64.tar.gz"
 
 pushd "j${J_VERSION}/bin"
   sudo bash -eux install-usr.sh
 
-  echo "install 'all'" >> install.ijs
-  echo "exit ''" >> install.ijs
-  sudo ijconsole install.ijs
-
+  sudo ijconsole -js "install 'all'" -js "exit''"
 popd
 
-rm -rf "j${J_VERSION}_linux64.tar.gz" "j${J_VERSION}" install.ijs
+rm -rf "j${J_VERSION}_linux64.tar.gz" "j${J_VERSION}"
 
